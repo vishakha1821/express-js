1230f57b5b03f08f5e38c2552bdb1398987723ab
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-05-23 19:36:00 +0530

    Add files via upload

--- express js 2/app.js
+++ express js 2/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const http = require('http');
 
 const express = require('express');
@@ -6,20 +8,17 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-
 const adminRoutes = require('/admin'); //IMPORTING ROUTER
 const shopRoutes = require('.shop.js');//IMPORTING SHOP ROUTER
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
-
 
 const server = http.createServer(app);
 server.listen(3000);
