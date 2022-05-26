8431254521e566bdba548dd382ff561df6276bf2
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-05-26 19:05:34 +0530

    Add files via upload

--- EJ/controllers/admin.js
+++ EJ/controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -19,6 +17,43 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next)=>{
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDsc = req.body.description;
+  const updatedProduct = new Product(prodId, 
+    updatedTitle,
+     updatedPrice,
+      updatedImageUrl,
+       updatedDsc,
+        updatedImageUrl);
+
+  updatedProduct.save();
+  res.redirect('/admin/products');
+
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
