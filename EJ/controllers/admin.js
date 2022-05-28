204e32145b514e039f7f7fb21124018b721d14c2
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-05-28 20:00:00 +0530

    Add files via upload

--- EJ/controllers/admin.js
+++ EJ/controllers/admin.js
@@ -13,9 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, imageUrl, description, price);
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -37,23 +41,22 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-exports.postEditProduct = (req, res, next)=>{
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
-  const updatedDsc = req.body.description;
-  const updatedProduct = new Product(prodId, 
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
     updatedTitle,
-     updatedPrice,
-      updatedImageUrl,
-       updatedDsc,
-        updatedImageUrl);
-
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
   updatedProduct.save();
   res.redirect('/admin/products');
-
-}
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -63,4 +66,10 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
